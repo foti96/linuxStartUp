bd050e970597a2b2bfadb432c79466e3c8026d74
Josh Foti <josh.foti96@gmail.com>
2020-09-14 21:21:39 +0800

    toolbox changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-apt update
-apt upgrade
+apt update -y
+apt upgrade -y
 apt install tilix git zsh curl -y
 snap install --classic code
 apt-get install \
@@ -18,20 +18,8 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 usermod -aG docker $USER 
 curl -L "https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-TOOLBOX_URL=$(curl --silent 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' \
-    -H 'Origin: https://www.jetbrains.com' \
-    -H 'Accept-Encoding: gzip, deflate, br' \
-    -H 'Accept-Language: en-US,en;q=0.8' \
-    -H 'Accept: application/json, text/javascript, */*; q=0.01' \
-    -H 'Referer: https://www.jetbrains.com/toolbox/download/' \
-    -H 'Connection: keep-alive' \
-    -H 'DNT: 1' \
-    --compressed \
-  | grep -Po '"linux":.*?[^\\]",' \
-  | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
 
-install -d ${HOME}/bin
-curl -sL ${TOOLBOX_URL} | tar xvz --directory=${HOME}/bin --strip-components=1
-# intial setup
-$(jetbrains-toolbox)
+curl -fsSL https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7455.tar.gz
+sudo tar -xzf jetbrains-toolbox-1.13.4801.tar.gz -C /opt
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
