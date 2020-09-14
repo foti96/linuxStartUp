a00b6309e3bc96d6ea1f17861394bef8b2a3ffbf
Josh Foti <josh.foti96@gmail.com>
2020-09-14 21:54:43 +0800

    chrome added

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 apt update -y
 apt upgrade -y
-apt install tilix git zsh curl -y
+apt install tilix git zsh curl tweaks gnome-shell=extensions chrome-gnome-shell -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
 snap install --classic code
+
 apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -19,7 +22,7 @@ usermod -aG docker $USER
 curl -L "https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-https://download.jetbrains.com/product?code=tb&latest&distribution=linux --output toolbox.tar.gz
-sudo tar -xzf toolbox.tar.gz -C /opt
+curl -L https://download.jetbrains.com/product?code=tb&latest&distribution=linux --output toolbox.tar.gz
+tar -xzf toolbox.tar.gz -C /opt
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
